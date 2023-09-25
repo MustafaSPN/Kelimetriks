e41d96f9e516faf35d36760b0570d8b7200a7f1f
MustafaSPN <mustafaspn@hotmail.com>
2023-09-25 15:41:35 +0300

    Add Localization Turkish and English UI

---
+++ Assets/Real Assets/Scripts/UI/LocalButtons.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LocalButtons : MonoBehaviour
+{
+    private void Awake()
+    {
+        int locale = 123;
+        locale = PlayerPrefs.GetInt("KELIMETRIKS_LOCALE_SETTINGS");
+        if (locale ==0 || locale==1)
+        {
+            StartCoroutine(SetLocale(locale));
+        }
+        
+    }
+
+    public void Turkish()
+    {
+        StartCoroutine(SetLocale(1));
+    }
+
+    public void English()
+    {
+        StartCoroutine(SetLocale(0));
+    }
+
+    private IEnumerator SetLocale(int local)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[local];
+        PlayerPrefs.SetInt("KELIMETRIKS_LOCALE_SETTINGS",local);
+    }
+    
+}
